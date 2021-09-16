9cb31a32cb2617559e07d5d82852013cbd47b300
xiaolanglang <2145773+xiaolanglanglang@users.noreply.github.com>
2021-09-16 09:23:57 +0800

    feat: Catching errors.

--- custom_components/proxmox_virtual_machine/client.py
+++ custom_components/proxmox_virtual_machine/client.py
@@ -45,69 +45,87 @@ class ProxmoxClient:
             self._logger.info("release lock")
 
     async def shutdown_by_agent(self, node: str, vm_id: str) -> bool:
-        self._logger.info(f"shutdown by agent, node: ({node}), vm: ({vm_id})")
-        self._last_status = "stopping"
-        self._last_operation_time = time.time()
-        shutdown_url = (
-            f"{self._host}/api2/json//nodes/{node}/qemu/{vm_id}/agent/shutdown"
-        )
-        headers = {"CSRFPreventionToken": self._token}
-        cookies = {"PVEAuthCookie": self._ticket}
-        async with httpx.AsyncClient(verify=False) as client:
-            res = await client.post(
-                shutdown_url,
-                headers=headers,
-                cookies=cookies,
-                timeout=OPERATION_API_TIMEOUT,
+        try:
+            self._logger.info(f"shutdown by agent, node: ({node}), vm: ({vm_id})")
+            self._last_status = "stopping"
+            self._last_operation_time = time.time()
+            shutdown_url = (
+                f"{self._host}/api2/json//nodes/{node}/qemu/{vm_id}/agent/shutdown"
             )
-        if res.status_code == 401:
-            login_result = await self.login()
-            if not login_result:
-                return False
-            return await self.shutdown_by_agent(node, vm_id)
-
-        return True
+            headers = {"CSRFPreventionToken": self._token}
+            cookies = {"PVEAuthCookie": self._ticket}
+            async with httpx.AsyncClient(verify=False) as client:
+                res = await client.post(
+                    shutdown_url,
+                    headers=headers,
+                    cookies=cookies,
+                    timeout=OPERATION_API_TIMEOUT,
+                )
+            if res.status_code == 401:
+                login_result = await self.login()
+                if not login_result:
+                    return False
+                return await self.shutdown_by_agent(node, vm_id)
+            return True
+        except:
+            self._logger.warning(
+                f"shutdown machine error, node: ({node}), vm: ({vm_id}"
+            )
+            return False
 
     async def start(self, node: str, vm_id: str) -> bool:
-        start_url = f"{self._host}/api2/json//nodes/{node}/qemu/{vm_id}/status/start"
-        self._last_status = "running"
-        self._last_operation_time = time.time()
-        headers = {"CSRFPreventionToken": self._token}
-        cookies = {"PVEAuthCookie": self._ticket}
-        async with httpx.AsyncClient(verify=False) as client:
-            res = await client.post(
-                start_url,
-                headers=headers,
-                cookies=cookies,
-                timeout=OPERATION_API_TIMEOUT,
+        try:
+            start_url = (
+                f"{self._host}/api2/json//nodes/{node}/qemu/{vm_id}/status/start"
             )
-        if res.status_code == 401:
-            login_result = await self.login()
-            if not login_result:
-                return False
-            return await self.start(node, vm_id)
-
-        return True
+            self._last_status = "running"
+            self._last_operation_time = time.time()
+            headers = {"CSRFPreventionToken": self._token}
+            cookies = {"PVEAuthCookie": self._ticket}
+            async with httpx.AsyncClient(verify=False) as client:
+                res = await client.post(
+                    start_url,
+                    headers=headers,
+                    cookies=cookies,
+                    timeout=OPERATION_API_TIMEOUT,
+                )
+            if res.status_code == 401:
+                login_result = await self.login()
+                if not login_result:
+                    return False
+                return await self.start(node, vm_id)
+            return True
+        except:
+            self._logger.warning(f"start machine error, node: ({node}), vm: ({vm_id}")
+            return False
 
     async def status(self, node: str, vm_id: str):
-        self._logger.debug(f"get status, node: ({node}), vm: ({vm_id})")
-        status_url = f"{self._host}/api2/json//nodes/{node}/qemu/{vm_id}/status/current"
-        cookies = {"PVEAuthCookie": self._ticket}
-        async with httpx.AsyncClient(verify=False) as client:
-            res = await client.get(status_url, cookies=cookies)
-        if res.status_code == 401:
-            login_result = await self.login()
-            if not login_result:
-                return False
-            return await self.status(node, vm_id)
-        result = res.json()
-        response_data = result["data"]
-        status = response_data["status"]
-        if time.time() - self._last_operation_time < DEFAULT_MIN_INTERVAL:
-            if self._last_status == "stopping" and status == "running":
-                return "stopping"
-            if self._last_status == "running" and status == "stopped":
-                return "running"
-        self._last_status = status
-        self._logger.debug(f"node: ({node}), vm: ({vm_id}), status: ({status})")
-        return status
+        try:
+            self._logger.debug(f"get status, node: ({node}), vm: ({vm_id})")
+            status_url = (
+                f"{self._host}/api2/json//nodes/{node}/qemu/{vm_id}/status/current"
+            )
+            cookies = {"PVEAuthCookie": self._ticket}
+            async with httpx.AsyncClient(verify=False) as client:
+                res = await client.get(status_url, cookies=cookies)
+            if res.status_code == 401:
+                login_result = await self.login()
+                if not login_result:
+                    return False
+                return await self.status(node, vm_id)
+            result = res.json()
+            response_data = result["data"]
+            status = response_data["status"]
+            if time.time() - self._last_operation_time < DEFAULT_MIN_INTERVAL:
+                if self._last_status == "stopping" and status == "running":
+                    return "stopping"
+                if self._last_status == "running" and status == "stopped":
+                    return "running"
+            self._last_status = status
+            self._logger.debug(f"node: ({node}), vm: ({vm_id}), status: ({status})")
+            return status
+        except:
+            self._logger.warning(
+                f"get machine status error, node: ({node}), vm: ({vm_id}"
+            )
+            return False
